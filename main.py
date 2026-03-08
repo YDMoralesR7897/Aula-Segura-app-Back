3f465faaad4e2d3b1f52070f550f46018f81e59f
Yerson <yerson7897@gmail.com>
2026-03-08 13:33:32 -0500

    querys prueba en main.py

--- main.py
+++ main.py
@@ -58,3 +58,80 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+
+"""
+create database aula_segura;
+use aula_segura;
+select * from perfil;
+select * from persona;
+select * from usuario;
+select * from hoja_vida;
+select * from orientacion_no_clinica;
+select * from tipo_persona;
+select * from detalle_hoja_vida;
+CREATE TABLE tipo_persona (
+    id_tipop INT AUTO_INCREMENT PRIMARY KEY,
+    nombretp VARCHAR(100) NOT NULL,
+    descripciontp TEXT,
+    estado BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO tipo_persona (nombretp, descripciontp)
+VALUES
+('Psicólogo', 'Profesional de apoyo emocional'),
+('Docente', 'Profesor de la institución'),
+('Orientador', 'Encargado de convivencia escolar');
+
+CREATE TABLE criterio_alerta (
+    id_criterio INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    estado BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO criterio_alerta (nombre)
+VALUES
+('Bullying'),
+('Aislamiento social'),
+('Bajo rendimiento académico');
+
+CREATE TABLE orientacion_no_clinica (
+    id_orientacion INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    estado BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO orientacion_no_clinica (nombre)
+VALUES
+('Recomendación pedagógica'),
+('Citación a padres'),
+('Actividad de integración');
+
+CREATE TABLE hoja_vida (
+    id_hoja INT AUTO_INCREMENT PRIMARY KEY,
+    id_persona INT,
+    fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,
+    estado BOOLEAN DEFAULT TRUE,
+
+    FOREIGN KEY (id_persona) REFERENCES persona(id_persona)
+);
+
+CREATE TABLE detalle_hoja_vida (
+    id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+    id_hoja INT,
+    fecha DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    id_criterio INT,
+    id_orientacion INT,
+    id_tipop INT,
+
+    observaciones TEXT,
+    persona_registra INT,
+
+    FOREIGN KEY (id_hoja) REFERENCES hoja_vida(id_hoja),
+    FOREIGN KEY (id_criterio) REFERENCES criterio_alerta(id_criterio),
+    FOREIGN KEY (id_orientacion) REFERENCES orientacion_no_clinica(id_orientacion),
+    FOREIGN KEY (id_tipop) REFERENCES tipo_persona(id_tipop),
+    FOREIGN KEY (persona_registra) REFERENCES persona(id_persona)
+);
+"""
